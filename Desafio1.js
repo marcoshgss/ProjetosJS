7a81c7c90f35b556fa95ee7ccce0b11547a0768d
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-06-22 20:26:56 -0300

    mais implementações

--- Desafio1.js
+++ Desafio1.js
@@ -33,7 +33,7 @@ function mudarIdade(idade1, idade2) {
     atual = idade1 + ' ' + idade2
     return atual
 }
-console.log(mudarIdade(5,10))
+console.log(mudarIdade(5, 10))
 console.log(atual)
 
 // SOMANDO NÚMEROS
@@ -41,7 +41,7 @@ function somarNum(n1, n2, n3) {
     console.log(arguments)
     return n1 + n2 + n3
 }
-console.log(somarNum(20,20,20))
+console.log(somarNum(20, 20, 20))
 
 // FAÇA DE OUTRA FORMA SIMPLES
 const n1 = 4
@@ -51,7 +51,58 @@ const multiplicando = n1 * n2 * n3
 console.log(multiplicando)
 
 function somasdosNumeros(n1, n2, n3) {
-    let result = n1+n2+n3
+    let result = n1 + n2 + n3
     return result
 }
-console.log(somasdosNumeros(100,200,200))
+console.log(somasdosNumeros(100, 200, 200))
+
+
+// OPERADOR TERNÁRIO 
+let pontos = 100
+let tipo = pontos > 100 ? 'premium' : 'comum'
+console.log(tipo)
+
+// OPERADORES LÓGICOS: &&, ||, !
+
+let maiorDeIdade = false
+let possuiCarteiraDeTrabalho = true
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalho
+let result = !podeAplicar
+console.log(result)
+
+
+// TROQUE O VALOR DE VERMELHO PARA AZUL
+let a = 'vermelho'
+let b = 'azul'
+
+let c = a
+a = b
+b = c
+console.log(a)
+console.log(b)
+
+
+// IF ELSE // SWICHT CASE
+
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até 18:00 : Boa tarde!
+// Caso contrário : Boa noite!
+
+let hora = 13
+if (hora > 6 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
+
+// LAÇO FOR, LAÇO WHILE, DO...WHILE
+
+for (let i = 0; i < 8; i++) {
+    console.log('estou estudando', i)
+}
+
+for (let i = 0; i <= 2; i++) {
+    console.log('estou estudando', i)
+}

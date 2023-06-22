f6125a9643258f9458307d1c5a715209c3becadc
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-06-21 23:05:30 -0300

    iniciando nos desafios JS - primeiro concluído

---
+++ Desafio1.js
@@ -0,0 +1,17 @@
+// IMPRIMIR 3 VARIÁVEIS DIFERENTES
+
+let dados = {
+    nome: "maria",
+    idade: 38,
+    sexo: "feminina"
+}
+
+console.log(dados)
+
+let nome = "Patricia"
+let sexo = "F"
+let cidade = "Recife"
+
+console.log(nome)
+console.log(sexo)
+console.log(cidade)

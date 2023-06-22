ad65d97d79f1ab1814ac64381fdb7d89384bb4fe
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-06-22 14:15:21 -0300

    mais implementações

--- Desafio1.js
+++ Desafio1.js
@@ -25,3 +25,33 @@ console.log(amigo[0])
 console.log(amigo[1])
 console.log(amigo[2])
 
+// APRENDENDO E IMPLEMENTANDO FUNÇÕES
+
+let atual = 40
+
+function mudarIdade(idade1, idade2) {
+    atual = idade1 + ' ' + idade2
+    return atual
+}
+console.log(mudarIdade(5,10))
+console.log(atual)
+
+// SOMANDO NÚMEROS
+function somarNum(n1, n2, n3) {
+    console.log(arguments)
+    return n1 + n2 + n3
+}
+console.log(somarNum(20,20,20))
+
+// FAÇA DE OUTRA FORMA SIMPLES
+const n1 = 4
+const n2 = 4
+const n3 = 4
+const multiplicando = n1 * n2 * n3
+console.log(multiplicando)
+
+function somasdosNumeros(n1, n2, n3) {
+    let result = n1+n2+n3
+    return result
+}
+console.log(somasdosNumeros(100,200,200))

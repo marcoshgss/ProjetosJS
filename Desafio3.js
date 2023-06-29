37ccace8e4b93932f7a33a9e994270bc1047cc09
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-06-29 06:18:31 -0300

    mais implementações

--- Desafio3.js
+++ Desafio3.js
@@ -30,12 +30,28 @@ console.log(celular1)
 
 // Pascal Case
 function Celular(marcaCelular,tamanhoTela,capacidadeBateria){
-    this.marcaCelular,
-    this.tamanhoTela,
-    this.capacidadeBateria,
+    this.marcaCelular = marcaCelular,
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
     this.ligar = function(){
-        console.log("Fazendo Ligação...")
+        console.log("Fazendo Ligação...");
     }
 }
 
-const celular2 = new Celular('Xiome', 6.4, 6000)
+const celular2 = new Celular('Xiaomi', 6.4, 6000);
+console.log(celular2)
+
+function Carro(fabricacao, modelo, ano, proprietario,mostreCarro) {
+    this.fabricacao = fabricacao,
+    this.modelo = modelo,
+    this.ano = ano,
+    this.proprietario = proprietario,
+    this.mostreCarro = mostreCarro,
+    this.retornar = function() {
+        console.log()
+    }
+  }
+
+const carro = new Carro('nacional', 'Gol', 2015, 'João Pedro', null);
+console.log(carro)
+

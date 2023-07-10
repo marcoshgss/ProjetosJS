29f5fe27083a835236b4152f9bfea150178e4d62
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-07-10 06:02:39 -0300

    mais implementação

--- estudando-novamente-js/variaveis.js
+++ estudando-novamente-js/variaveis.js
@@ -1,2 +1,30 @@
-let mensagem = "Olá, mundo!";
+let mensagem = "Olá, mundo!"; // String literal
 console.log(mensagem);
+
+let idade = 20; // number literal
+console.log(idade)
+
+let estaAprovado = true; // boolean
+console.log(estaAprovado)
+
+let sobrenome = undefined; // Undefined
+console.log(sobrenome)
+
+let corSelecionado = null; // Redefinir um valor
+console.log(corSelecionado)
+
+// OBJETOS
+
+let pessoa = {
+    nome: 'Pedro',
+    sobrenome: 'Gonçalve',
+    idade: 16,
+    cpf: 15464845411,
+    rg:44458765,
+    nacionalidade: 'Brasileiro',
+    estado: 'São Paulo',
+    cidade: 'Osasco'
+}
+
+console.log(pessoa)
+console.log(pessoa.cpf)

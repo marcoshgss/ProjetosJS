ee61d0897600739a9f04c077cafca305b97a0913
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-07-10 07:59:19 -0300

    mais implementação

--- estudando-novamente-js/operadoresLogicos.js
+++ estudando-novamente-js/operadoresLogicos.js
@@ -2,3 +2,14 @@
 let pontos = 300
 let tipo = pontos > 100 ? 'Premium' : 'comum'
 console.log(tipo)
+
+// Operadores lógicos e(&&) ou(||)
+
+let maiorDeIdade = true
+let possueCarteiraDeTrabalho = false
+let podeAplicarPraVaga = maiorDeIdade && possueCarteiraDeTrabalho
+let podeAplicarPraVaga1 = maiorDeIdade || possueCarteiraDeTrabalho
+console.log('Pode Aplicar', podeAplicarPraVaga)
+
+let candidatoRecusado = !podeAplicarPraVaga
+console.log('Candidato Recusado', candidatoRecusado)

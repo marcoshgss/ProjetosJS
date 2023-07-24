130e1ebd063ecacc42cd8749a03a413cad7a20a6
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-07-24 07:39:04 -0300

    mais implementações

--- Desafio8.js
+++ Desafio8.js
@@ -30,17 +30,42 @@ const object = {
 }
 
 // Desafio, verificar cada elemento do object, se estiver null, apague esse elemento e mostre o restante.
-function excluirItemObject(object) {
-    if (object.nome === null) {
-        delete object.nome
-    } else if (object.sobrenome === null){
-        delete object.sobrenome
-    } else if (object.idade === null){
-        delete object.idade
-    }
-    console.log(object)
-}
+// function excluirItemObject(object) {
+//     if (object.nome === null) {
+//         delete object.nome
+//     } else if (object.sobrenome === null) {
+//         delete object.sobrenome
+//     } else if (object.idade === null) {
+//         delete object.idade
+//     }
+//     console.log(object)
+// }
 
-excluirItemObject(object)
+// excluirItemObject(object)
 
 
+// Desafio, função que recebe um array de objetos com atributos : itsMe, admin, e nome.
+
+const arrayObj = [
+    { itsMe: false, admin: false, nome: 'Maria' },
+    { itsMe: true, admin: true, nome: 'Marcos' }, // a
+    { itsMe: false, admin: false, nome: 'Adelson' },
+    { itsMe: false, admin: false, nome: 'Pedro' },
+    { itsMe: false, admin: false, nome: 'Eduarda' }
+];
+
+
+const orgazinar = (objList) => {
+    objList.sort((a, b) => {
+        if (a.itsMe === true && b.itsMe === false) {
+            return -1
+        }
+    });
+    console.log(objList);
+};
+
+orgazinar(arrayObj)
+
+// Desafio, função que recebe um número e um array de números. Essa função deve retornar o número de array que for mais
+// próximo do primeiro número passado por parâmetro.
+ 
